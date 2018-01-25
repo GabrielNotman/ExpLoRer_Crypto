c5aec7c0bbd7c0412de08e5338002417ddae6b50
gabriel <g.notman@gmail.com>
2018-01-25 16:36:09 +0000

    Updated script output

--- scripts/genKeyPair.sh
+++ scripts/genKeyPair.sh
@@ -13,7 +13,7 @@ echo "Generating private key: "$fname"_priv.pem"
 openssl ecparam -name prime256v1 -genkey -noout -out $fname"_priv.pem"
 echo "Deriving public key: "$fname"_pub.pem"
 openssl ec -in $fname"_priv.pem" -pubout -out $fname"_pub.pem" 2>/dev/null
-echo "Extracting ECC public key: "
+echo "Extracting EC public key: "
 openssl ec -pubin -in $fname"_pub.pem" -outform DER 2>/dev/null | xxd -p -s27 -c256 -u
 
 
