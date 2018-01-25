cf9273d37c61b19e3435f670e37cc591edae66ad
gabriel <g.notman@gmail.com>
2018-01-25 23:55:15 +0000

    Updated genKeyPair
    Confirmed curve type

--- scripts/genKeyPair.sh
+++ scripts/genKeyPair.sh
@@ -7,10 +7,12 @@ else
     fname=$1
 fi
 
+keytypename="prime256v1"
+
 export RANDFILE=./.rnd
-echo "Generating EC key pair using prime256v1"
+echo "Generating EC key pair using $keytypename"
 echo "Generating private key: "$fname"_priv.pem"
-openssl ecparam -name prime256v1 -genkey -noout -out $fname"_priv.pem"
+openssl ecparam -name $keytypename -genkey -noout -out $fname"_priv.pem"
 echo "Deriving public key: "$fname"_pub.pem"
 openssl ec -in $fname"_priv.pem" -pubout -out $fname"_pub.pem" 2>/dev/null
 echo "Extracting EC public key: "
