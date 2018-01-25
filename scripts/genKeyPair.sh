573db931ca373e6fc5587bc9d03dbee619f5c3da
gabriel <g.notman@gmail.com>
2018-01-25 14:37:20 +0000

    Updated scripts

--- scripts/genKeyPair.sh
+++ scripts/genKeyPair.sh
@@ -4,10 +4,13 @@ if [ $# -ne 1 ]
 then
     echo "Correct usage: ./genKeyPair.sh <keyName>"
 else
-    set -x #echo on
     export RANDFILE=./.rnd
-    openssl ecparam -name secp256k1 -genkey -noout -out $1_priv.pem
-    openssl ec -in $1_priv.pem -pubout -out $1_pub.pem
-    cat $1_pub.pem
+    echo "Generating EC key pair using prime256v1"
+    echo "Generating private key: "$1"_priv.pem"
+    openssl ecparam -name prime256v1 -genkey -noout -out $1_priv.pem
+    echo "Deriving public key: "$1"_pub.pem"
+    openssl ec -in $1_priv.pem -pubout -out $1_pub.pem 2>/dev/null
+    echo "Extracting ECC public key: "
+    openssl ec -pubin -in $1_pub.pem -outform DER 2>/dev/null | xxd -p -s27 -c256 -u
 fi
 
--- scripts/genSecret.sh
+++ scripts/genSecret.sh
@@ -4,7 +4,9 @@ if [ $# -ne 2 ]
 then
     echo "Correct usage: ./genSecret.sh <privateName> <publicName>"
 else
-    set -x #echo on
+    echo "Generating shared secret using: $1_priv.pem & $2_pub.pem"    
+    echo "Saving shared secreted to: $1_$2_secret.bin"
     openssl pkeyutl -derive -inkey $1_priv.pem -peerkey $2_pub.pem -out $1_$2_secret.bin
-    base64 $1_$2_secret.bin
+    echo "Hex dump of $1_$2_secret.bin: "
+    xxd -p -u -c256 < $1_$2_secret.bin
 fi
