ff443e9450a9a3117cbfd30e6774484178b46c0f
gabriel <g.notman@gmail.com>
2018-01-26 19:38:15 +0000

    Updated decryptCBC script

--- scripts/decryptCBC.sh
+++ scripts/decryptCBC.sh
@@ -9,8 +9,8 @@ echo -n "Enter IV HEX: "
 read iv
 
 echo -n "Plain text: "
-set -x
-openssl enc -aes-256-cbc -nosalt -nopad -iv $iv --K $(xxd -p -u -c256 ./host_device_secret.bin) -d -in cipher.tmp -out plain.tmp
+
+openssl enc -aes-256-cbc -nosalt -nopad -iv $iv -K $(xxd -p -u -c256 ./host_device_secret.bin) -d -in cipher.tmp -out plain.tmp
 echo "" >> plain.tmp
 cat plain.tmp
 
