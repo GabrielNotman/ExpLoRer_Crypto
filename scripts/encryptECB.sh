85ec423a8752ee0bd74c3f7cd51e00a52a0a1177
gabriel <g.notman@gmail.com>
2018-01-26 19:20:31 +0000

    Updated ECB scripts

--- scripts/encryptECB.sh
+++ scripts/encryptECB.sh
@@ -4,7 +4,7 @@ echo "Testing encrypting with AES-256 ECB mode"
 echo -n "Enter plain text: "
 read plaintext
 echo -n $plaintext > plain.tmp
-#set -x
+
 echo "Cipher text:"
 openssl enc -aes-256-ecb -nosalt -K $(xxd -p -u -c256 host_device_secret.bin) -e -in plain.tmp | xxd -p -c256 -u
 
