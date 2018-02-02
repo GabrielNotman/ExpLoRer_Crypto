3016cb1f5f44d8ad79a0488f179ed33a3bf9d867
Gabriel Notman <g.notman@gmail.com>
2018-02-02 09:51:12 +0000

    Updated decryptCBC script

--- scripts/decryptCBC.sh
+++ scripts/decryptCBC.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 echo "Testing decrypting with AES-256 CBC mode"
-echo -n "Enter cipher HEX: "
-read cipher
-echo $cipher | xxd -r -p > cipher.tmp
 
 echo -n "Enter IV HEX: "
 read iv
 
+echo -n "Enter cipher HEX: "
+read cipher
+echo $cipher | xxd -r -p > cipher.tmp
+
 echo -n "Plain text: "
 
-openssl enc -aes-256-cbc -nosalt -nopad -iv $iv -K $(xxd -p -u -c256 ./host_device_secret.bin) -d -in cipher.tmp -out plain.tmp
+openssl enc -aes-256-cbc -nosalt -iv $iv -K $(xxd -p -u -c256 ./host_device_secret.bin) -d -in cipher.tmp -out plain.tmp
 echo "" >> plain.tmp
 cat plain.tmp
 
